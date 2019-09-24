52e5dff5291ad8fac17d1e64d04f673510a46016
glaphire <motoryna.dariia@pdffiller.team>
2019-09-25 00:17:54 +0300

    refactored validation form frontend

--- resources/views/ogrn/validation_form.blade.php
+++ resources/views/ogrn/validation_form.blade.php
@@ -1,22 +1,24 @@
 @extends('ogrn.page_layout')
     @section('main-content')
     <form id="ogrn-validation-form">
+        <label for="ogrn_number">ОГРН:</label>
         <input type="text" name="ogrn_number" value="" placeholder="Введите ОГРН">
         <span class="response-status"></span>
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <input type="submit" value="Проверить">
-        <input class="search" type="button" value="Найти">
+        <input name="validate" id="validate" type="button" value="Проверить">
+        <input name="search" id="search" type="button" value="Найти">
     </form>
     @endsection
     @section('bottom-scripts')
     <script type="text/javascript">
-        $('#ogrn-validation-form').submit(function(e) {
+        $('#validate').on('click', function(e) {
             e.preventDefault();
+            var ogrn_number = $("input[name=ogrn_number]").val();
             $.ajax({
                 type: "POST",
                 url: '/ogrn/validate',
                 dataType: 'json',
-                data: $(this).serialize(),
+                data: {'ogrn_number': ogrn_number},
                 success: function()
                 {
                     $('.response-status').html("&#10004; корректен").text();
@@ -27,7 +29,7 @@
             });
         });
 
-        $('.search').on('click', function(e) {
+        $('#search').on('click', function(e) {
             var ogrn_number = $("input[name=ogrn_number]").val();
             window.location = '/ogrn/show-currency-form?ogrn_number=' + ogrn_number;
         });
